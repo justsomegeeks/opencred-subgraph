15920f8691b423d539efecd252d498be5b61fc76
chiranz <chiranpoudyal@gmail.com>
2022-01-22 14:07:00 +0530

    Fix: index events (Review, CourseCreated, Graduate)

--- src/openCredFactory.ts
+++ src/openCredFactory.ts
@@ -1,21 +1,19 @@
-import {
-  BootcampCreated,
-  OwnershipTransferred,
-} from "../generated/OpenCredFactory/OpenCredFactory";
+import { BootcampCreated } from "../generated/OpenCredFactory/OpenCredFactory";
 import { Bootcamp } from "../generated/schema";
+import { OpenCred } from "../generated/templates";
 
 export function handleBootcampCreated(event: BootcampCreated): void {
-  let entity = new Bootcamp(event.params.bootcamp.toHex());
+  let bootcamp = new Bootcamp(event.params.bootcamp.toHex());
 
   // TODO: Handle ipfs
   // Entity fields can be set based on event parameters
 
-  entity.owner = event.params.owner;
-  entity.address = event.params.bootcamp;
-  entity.uri = event.params.bootcampURI;
+  bootcamp.owner = event.params.owner;
+  bootcamp.address = event.params.bootcamp;
+  bootcamp.uri = event.params.bootcampURI;
 
   // Entities can be written to the store with `.save()`
-  entity.save();
-}
+  bootcamp.save();
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  OpenCred.create(event.params.bootcamp);
+}
